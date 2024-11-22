818531c1e87c7d4973389b7e68377e331af31b88
Aaron Underhill <underhaa@oregonstate.edu>
2024-11-22 10:56:44 -0800

    feat: updating footer

--- components/layouts/footer.js
+++ components/layouts/footer.js
@@ -8,61 +8,68 @@ import Image from "next/image";
  *************************************************************************/
 function Footer() {
   return (
-    <div className={styles.footerframe}>
-      <div className={styles.footerInfoContainer}>
-        <Image
-          className={styles.osufootericon}
-          width={100}
-          height={100}
-          alt=""
-          src="/osufootericon.svg"
-        />
-        <b className={styles.collegeOfHealth}>
-          College Of Health ASP3IRE Center
-        </b>
-        <b className={styles.oregonStateUniversitys}>
-          Oregon State University’s Advancing Science, Practice, Programming and
-          Policy in Research Translation for Children&apos;s Environmental
-          Health Center
-        </b>
+    <>
+      <div className={styles.footerframe}>
+        {/* Left Section */}
+        <div className={styles.footerInfoContainer}>
+          <Image
+            className={styles.osufootericon}
+            width={200}
+            height={75}
+            alt="Oregon State University Logo"
+            src="/osufootericon.svg"
+          />
+          <div className={styles.collegeOfHealth}>
+            College Of Health ASP3IRE Center
+          </div>
+          <div className={styles.oregonStateUniversitys}>
+            Oregon State University’s Advancing Science, Practice, Programming
+            and Policy in Research Translation for Children&apos;s Environmental
+            Health Center
+          </div>
+        </div>
+
+        {/* Divider Line */}
+        <div className={styles.dividerlineIcon}></div>
+
+        {/* Right Section */}
+        <div className={styles.authorAndContributorContainer}>
+          <div className={styles.authorAndContibutorText}>Author</div>
+          <div className={styles.authorAndContibutorMemberText}>
+            Siew Sun Wong - Professor of Nutrition
+          </div>
+          <div className={styles.authorAndContibutorText}>Contributors</div>
+
+          <div className={styles.contributorsGrid}>
+            <div>Yaire Aguilar</div>
+            <div>Kyu Sung Kwon</div>
+            <div>Garrett Berliner</div>
+            <div>Anastasiia Ragozina</div>
+            <div>Taryn Eng</div>
+            <div>Abdulrahman Samargandi</div>
+            <div>Gerald Hendrix</div>
+            <div>Mitch Stephenson</div>
+            <div>Charissa Kau</div>
+            <div>Aaron Underhill</div>
+            <div>Emma Koehmstedt</div>
+          </div>
+
+        </div>
       </div>
-      <Image
-        className={styles.dividerlineIcon}
-        alt=""
-        src="/dividerline.svg"
-        width={100}
-        height={100}
-      />
-      <div className={styles.authorAndContributorContainer}>
-        <p className={styles.authorAndContibutorText}>Author</p>
-        <p className={styles.authorAndContibutorMemberText}>
-          Siew Sun Wong - Professor of Nutrition
-        </p>
-        <p className={styles.authorAndContibutorText}>Contibutors</p>
-        <p className={styles.authorAndContibutorMemberText}>Garrett Berliner</p>
-        <p className={styles.authorAndContibutorMemberText}>Gerald Hendrix</p>
-        <p className={styles.authorAndContibutorMemberText}>Emma Koehmstedt</p>
-        <p className={styles.authorAndContibutorMemberText}>Kyu Sung Kwon</p>
-        <p className={styles.authorAndContibutorMemberText}>Mitch Stephenson</p>
-      </div>
-      <Image
-        className={styles.dividerlineIcon}
-        alt=""
-        src="/dividerline.svg"
-        width={100}
-        height={100}
-      />
-      <div className={styles.authorAndContributorContainer}>
-        <p className={styles.communityTextBold}>Community</p>
+
+      {/* Bottom Bar */}
+      <div className={styles.footerBottomBar}>
+        <div className={styles.communityTitle}>Community</div>
         <a
           href="https://forms.gle/AsuCAf2kRhGhPp6S6"
           className={styles.feedbackLink}
         >
-          {" "}
-          feedback{" "}
+          Feedback
         </a>
       </div>
-    </div>
+
+
+    </>
   );
 }
 
